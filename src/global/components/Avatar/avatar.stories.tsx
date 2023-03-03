ad5e62f4f8a29eef3d8400fc2f8e82768a4c8970
Vinicius Italo <viniciusidacruz@hotmail.com>
2023-03-03 16:24:10 -0300

    feat: create stories for component Avatar

---
+++ src/global/components/Avatar/avatar.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { AvatarComponent } from ".";
+import { IComponentParams } from "./types";
+
+export default {
+  title: "Components/Avatar ",
+  component: AvatarComponent,
+} as Meta;
+
+export const Default: StoryObj<IComponentParams> = {
+  args: {
+    isprofilesquare: true,
+    label: "Vinicius",
+    alt: "Texto alternativo para testes",
+    src: "https://avatars.githubusercontent.com/u/84112057?v=4",
+  },
+};
