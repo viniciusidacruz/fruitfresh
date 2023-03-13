c826f792775474219e38543b07741f8f1f393fc0
Joyce_Bigoli <joycebigoli@gmail.com>
2023-03-13 12:16:42 -0300

    refactor: tag corretion

--- src/global/components/Logo/index.tsx
+++ src/global/components/Logo/index.tsx
@@ -6,7 +6,7 @@ export const LogoComponent = () => {
       className="select-none flex items-center"
       data-testid="group-element"
     >
-      <NavLink target="_self" to={"/home"} data-testid="link-element">
+      <NavLink to={"/"} data-testid="link-element">
         <img
           className=" w-15 h-15 rounded-full"
           data-testid="image-element"
@@ -14,12 +14,12 @@ export const LogoComponent = () => {
           alt="Imagem de uma melancia cortada ao meio"
         ></img>
 
-        <label
+        <span
           data-testid="label-element"
           className="text-xl text-lime-900 text-bold justify-center mt-2 select-none hidden sm:flex"
         >
           Fruit Fresh
-        </label>
+        </span>
       </NavLink>
     </figure>
   );
